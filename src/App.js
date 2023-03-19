e6721940d503c41638e93b9881a5538d03082ec0
Andrew Dunn <dunnad@gmail.com>
2023-03-19 22:26:56 +0000

    SPEED-12 Simplyfy state

--- src/App.js
+++ src/App.js
@@ -2,52 +2,51 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 import { VictoryAxis, VictoryChart, VictoryLine,} from 'victory';
 
+function GetDomainRange(records) {
+  var lowY = 1000
+  var highY  = 1
+
+  function updateDomainRange(record) {
+
+    if(record.DownloadSpeed > highY) {
+      highY = record.DownloadSpeed
+    }
+
+    if(record.DownloadSpeed < lowY) {
+      lowY = record.DownloadSpeed
+    }
+  }
+
+  records.forEach(updateDomainRange)
+  return [ lowY, highY ]
+}
+
 function App() {
 
   const [records, setRecords] = useState([])  
-  const [ready, setReady] = useState(false)
-  const [highY, setHighY] = useState(1)
-  const [lowY, setLowY] = useState(1000)
 
-  useEffect((lowY) => {
+  useEffect(() => {
     fetch("http://localhost:8080/records")
     .then((response) =>
       response.json()
     )
     .then((data) => {
       setRecords(data)
-      var lowY = 1000
-      var highY = 0
-
-      function updateDomainRange(record) {
-        
-        if(record.DownloadSpeed > highY) {
-          highY = record.DownloadSpeed
-        }
-
-        if(record.DownloadSpeed < lowY) {
-          lowY = record.DownloadSpeed
-        }
-      }
-      data.forEach(updateDomainRange)
-      setHighY(highY * 1.2)
-      setLowY(lowY * .8)
-      setReady(true)
     })
   }, [])
 
-  if (!ready) return <div>
-    <h1>Loading...</h1></div>
+  if (records === undefined || records === null || records.length === 0) return <div>
+    <h1>Waiting for some results...</h1></div>
 
   return (
     <div className="App">
-      <h1>t'Internet speed</h1>       
+      <h1>t'Internet speed</h1>
       <VictoryChart height={200} width={410} scale={{x: "time", y: "linear"}}>
           <VictoryLine          
           data = {records}
           x = "TimeStamp" 
           y = "DownloadSpeed"
-          domain={{x: [0, 100], y: [lowY, highY], }}/>
+          domain={{x: [0, 100], y: GetDomainRange(records)}}/>
           <VictoryAxis style={{
             tickLabels: {fontSize: 8, verticalAnchor:"start", angle:270},
             ticks: {stroke: "grey", size: 5},
